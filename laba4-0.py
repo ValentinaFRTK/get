3f6d9036d2dbb9c6a51eb6a23ddc698295aa77c8
Leontiuk Valentina <leontiuk.vv@phystech.edu>
2023-03-01 13:27:45 +0300

    adapt laba4-0.py for my RPi9

--- laba4-0.py
+++ laba4-0.py
@@ -1,17 +1,20 @@
 #An example to blink an LED once every two seconds:
 
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+dac = [24, 25]
+GPIO.setup(dac, GPIO.OUT)
 
-p = GPIO.PWM(12, 0.5)
+p = GPIO.PWM(24, 1000)
+p2 = GPIO.PWM(25, 100)
 p.start(1)
+p2.start(1)
 input('Press return to stop:')   # use raw_input for Python 2
 p.stop()
+p2.stop()
 GPIO.cleanup()
 
 
 
-k = 12.6543219
-
-print("{:.4f}".format(k))
+#k = 12.6543219
+#print("{:.4f}".format(k))
