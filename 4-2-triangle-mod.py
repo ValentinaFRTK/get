e91f7b72f79100fcd8c8e6a153e8c8d42677e83c
Valentina Leontiuk <leontiuk.vv@phystech.edu>
2023-03-08 13:10:38 +0300

    fixed triangle signal

--- 4-2-triangle-mod.py
+++ 4-2-triangle-mod.py
@@ -8,7 +8,31 @@ GPIO.setup(dac, GPIO.OUT)
 def decimal2binary(value):
 	return [int(element) for element in bin(value)[2:].zfill(8)]
 print("Для выхода нажмите Ctrl+C\n")
-p = GPIO.PWM(21, 100)
+
+try:
+    T = int(input("Задайте период треугольного сигнала (с):\n"))
+    t = 1/512*T   #60% от Т горит, 40% - не горит
+    while(1):
+        try: 
+            for i in range(255):
+                GPIO.output(dac, decimal2binary(i))
+                time.sleep(t)
+            for i in range(255):
+                GPIO.output(dac, decimal2binary(255 - i))
+                time.sleep(t)
+                
+        except (TypeError, ValueError, RuntimeError):
+            test = "Неправильный ввод: введите целое число или q для выхода "
+            print(test)
+except KeyboardInterrupt:
+    print("Нажатие ctrl+c - выход")
+    quit()
+finally:
+    print("Работает блок finally")
+    GPIO.output(dac, 0)
+    GPIO.cleanup()
+
+'''p = GPIO.PWM(21, 100)
 
 try:
     T = int(input("Задайте период треугольного сигнала (с):\n"))
@@ -34,3 +58,4 @@ finally:
     print("Работает блок finally")
     GPIO.output(dac, 0)
     GPIO.cleanup()
+'''
