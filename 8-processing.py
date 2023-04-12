fa63b9ae3fa52025efe1716b340726c3ddbd4ed6
Leontiuk Valentina <leontiuk.vv@phystech.edu>
2023-04-12 13:53:17 +0300

    practically all complete, graph and calculations remained

--- 8-processing.py
+++ 8-processing.py
@@ -1,3 +1,14 @@
-file = open('data.txt', 'r')
-file.read()
-file.close()
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open('settings.txt', 'r') as settings:
+    tmp = [float(item) for item in settings.read().split("\n")]
+    print(tmp)
+
+data_array = np.loadtxt("data.txt", dtype = int)
+print(data_array)
+
+fig, ax = plt.subplots(figsize = (16, 10), dpi = 400)
+ax.plot(data_array)
+fig.savefig("test.png")
+plt.show()
